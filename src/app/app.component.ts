67c9195c0cec2685000a2286e22954c964de8722
Willy Chu <imshaowei@gmail.com>
2018-10-01 00:26:08 +0800

    完成練習

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,18 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  keywordCount = 0;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
     }
+  }
+  keyupAction($event: KeyboardEvent) {
+    if ($event.key === 'Escape') {
+      (<HTMLInputElement>$event.target).value = '';
+    }
+    this.keywordCount = (<HTMLInputElement>$event.target).value.length;
   }
 }
